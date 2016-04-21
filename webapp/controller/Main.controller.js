647b31c75758e657a8a372a31785c06f26620cce
Michael Englbrecht <michael.englbrecht@10point-software.com>
2016-04-21 19:16:35 +0000

    Initial Commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",	
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller,Filter, FilterOperator) {
+	"use strict";
+
+	return Controller.extend("sap.training.filterbar.controller.Main", {
+		
+		
+	});
+
+});
